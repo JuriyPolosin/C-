c506981e822f586537fa8b49b24122562f35e756
Juriy Polosin <polosin.juriy@yandex.ru>
2022-10-05 17:47:15 +0700

    Три задачи в одном файле, окончательный вариант

--- lectures/lectures_1/Program.cs
+++ lectures/lectures_1/Program.cs
@@ -1,4 +1,30 @@
-﻿//Задача про гири из прошлой темы
+﻿//Задача_1 на сложение двух рандомных чисел от 1 до 9 включительно с выводом на экран
+/*
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
+*/
+
+//Задача_2, приветствие любимчика
+/*
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+*/
+
+//Задача_3 про гири из прошлой темы
 
 int a = 1;
 int b = 2;
