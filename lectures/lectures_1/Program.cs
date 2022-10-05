41975bbe76ab2c0e1c4d817d849dfd36196af2ef
Juriy Polosin <polosin.juriy@yandex.ru>
2022-10-05 13:34:18 +0700

    Задача №1 сложение двух рандомных чисел

--- lectures/lectures_1/Program.cs
+++ lectures/lectures_1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача на сложение двух рандомных чисел от 1 до 9 включительно с выводом на экран
+
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
