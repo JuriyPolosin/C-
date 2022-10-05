586702be861aad8042de106db0697fc18c3cd634
Juriy Polosin <polosin.juriy@yandex.ru>
2022-10-05 14:10:36 +0700

    Лекция1 Задача2 Приветствие любимчика
    Лекция1 Задача3 Про гири из прошлых тем
    
    Лекция1 слияние последних двух коммитов

--- lectures/lectures_1/Program.cs
+++ lectures/lectures_1/Program.cs
@@ -1,8 +1,17 @@
-﻿//Задача на сложение двух рандомных чисел от 1 до 9 включительно с выводом на экран
+﻿//Задача про гири из прошлой темы
 
-int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 40;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
