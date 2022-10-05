13468145f188c81f73edb5cdfdd2de376a7503f9
Juriy Polosin <polosin.juriy@yandex.ru>
2022-10-05 14:25:56 +0700

    Лекция1 Задача3 Про гири из прошлых тем

--- lectures/lectures_1/Program.cs
+++ lectures/lectures_1/Program.cs
@@ -1,24 +1,17 @@
-﻿//Задача на сложение двух рандомных чисел от 1 до 9 включительно с выводом на экран
-/*
-int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
-*/
+﻿//Задача про гири из прошлой темы
 
-//Задача, приветствие любимчика
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 40;
 
-Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
 
-if (username.ToLower() == "маша")
-{
-    Console.WriteLine("Ура, это же МАША!!!");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+Console.Write("max = ");
+Console.WriteLine(max);
