7fa1874dbaddf8c28d41082c568d2789079a6d78
Juriy Polosin <polosin.juriy@yandex.ru>
2022-10-20 15:39:54 +0700

    Домашнее задание №7

---
+++ homework/homework_7/Program.cs
@@ -0,0 +1,87 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+/*
+int m = 7, n = 7;
+double[,] array = new double[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        double t = new Random().NextDouble() * 20 - 10;
+        array[i, j] = Math.Round(t, 1);
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+        Console.Write($"{array[i, j]} ");
+    Console.WriteLine();
+}
+*/
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+/*
+int m = 3, n = 3;
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(20);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int a, b;
+int.TryParse(Console.ReadLine()!, out a);
+b = a % 10;
+a /= 10;
+
+if (a >= array.GetLength(0) || b >= array.GetLength(1) || a < 0)
+    Console.WriteLine($"Выход за границы массива!");
+else
+{
+    Console.WriteLine($"{array[a, b]}");
+}
+*/
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int m = 4, n = 4;
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+for (int j = 0; j < n; j++)
+{
+    int sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"Среднее арифметическое {j + 1} столбца = {(double)sum / m}\n");
+}
